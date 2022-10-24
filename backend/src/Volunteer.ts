b1e21551d0a7e461d842d62085814acb3f532254
KihyeokK <joe.brighter20@gmail.com>
2022-10-24 19:27:31 -0400

    Create Vounteer DB Class

---
+++ backend/src/Volunteer.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+// Creates an entity
+// Entity is a class that maps to a database table : https://orkhan.gitbook.io/typeorm/docs/entities
+@Entity()
+export class Volunteer extends User {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	phoneNumber: number;
+
+	@Column()
+	startDate: string;
+
+	@Column()
+	profilePicture: string;
+}
