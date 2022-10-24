197a3c380b6d44c39a64e8039fc8030add39da43
Lily Gostovic <lily.gostovic@gmail.com>
2022-10-24 19:08:45 -0400

    Create user entity

---
+++ backend/src/User.ts
@@ -0,0 +1,23 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
