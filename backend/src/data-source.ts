7f689ff1537c60a9d71ceed590e94a39378cb441
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2022-10-24 21:24:27 -0400

    Included AdminEntity in entities list of data-source.ts

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { AdminEntity } from "./entities/AdminEntity";
 import { ExampleEntity } from "./ExampleEntity";
 
 // Create a data source i.e connection settings: https://orkhan.gitbook.io/typeorm/docs/data-source#what-is-datasource
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
 	database: "test",
 	synchronize: true,
 	logging: false,
-	entities: [ExampleEntity],
+	entities: [ExampleEntity, AdminEntity],
 	migrations: [],
 	subscribers: [],
 });
