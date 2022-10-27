d10fec10e35e2d6239e6c443eb97c6b055749717
Peter Rong <peter.gh.rong@gmail.com>
2022-10-26 20:00:59 -0400

    test

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,18 +1,18 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { ExampleEntity } from "./ExampleEntity";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { ExampleEntity } from './ExampleEntity';
 
 // Create a data source i.e connection settings: https://orkhan.gitbook.io/typeorm/docs/data-source#what-is-datasource
 export const AppDataSource = new DataSource({
-	type: "postgres",
-	host: "localhost",
-	port: 5432,
-	username: "test",
-	password: "test",
-	database: "test",
-	synchronize: true,
-	logging: false,
-	entities: [ExampleEntity],
-	migrations: [],
-	subscribers: [],
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'test',
+  password: 'tes',
+  database: 'test',
+  synchronize: true,
+  logging: false,
+  entities: [ExampleEntity],
+  migrations: [],
+  subscribers: []
 });
