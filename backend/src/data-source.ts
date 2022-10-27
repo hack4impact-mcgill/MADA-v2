9452e26be914bd6611553290d4f66b0f6c8d158e
Peter Rong <peter.gh.rong@gmail.com>
2022-10-26 20:15:56 -0400

    update

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   host: 'localhost',
   port: 5432,
   username: 'test',
-  password: 'tes',
+  password: 'test',
   database: 'test',
   synchronize: true,
   logging: false,
