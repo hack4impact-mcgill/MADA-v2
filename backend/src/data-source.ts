03c15dc4873330e35bb8ad5965afcc27c208fa65
Hamza Farooq <hamzafarooq360@gmail.com>
2022-10-31 19:15:32 -0400

    ran prettier

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,19 +1,19 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { Client } from "./entities/ClientEntity";
-import { ExampleEntity } from "./ExampleEntity";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Client } from './entities/ClientEntity';
+import { ExampleEntity } from './ExampleEntity';
 
 // Create a data source i.e connection settings: https://orkhan.gitbook.io/typeorm/docs/data-source#what-is-datasource
 export const AppDataSource = new DataSource({
-	type: "postgres",
-	host: "localhost",
-	port: 5432,
-	username: "test",
-	password: "test",
-	database: "test",
-	synchronize: true,
-	logging: false,
-	entities: [Client],
-	migrations: [],
-	subscribers: [],
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'test',
+  password: 'test',
+  database: 'test',
+  synchronize: true,
+  logging: false,
+  entities: [Client],
+  migrations: [],
+  subscribers: []
 });
--- backend/src/entities/ClientEntity.ts
+++ backend/src/entities/ClientEntity.ts
@@ -1,25 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 // Creates an entity
 // Entity is a class that maps to a database table : https://orkhan.gitbook.io/typeorm/docs/entities
 
 @Entity()
 export class Client {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Column()
-	address: string;
+  @Column()
+  address: string;
 
-    @Column()
-	email: string;
+  @Column()
+  email: string;
 
-    @Column()
-	phoneNumber: Number;
+  @Column()
+  phoneNumber: Number;
 
-    @Column()
-	name: string;
+  @Column()
+  name: string;
 
-    @Column()
-	notes: string;
+  @Column()
+  notes: string;
 }
