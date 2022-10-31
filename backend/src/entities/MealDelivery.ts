31f02af353d0ab7d7fb942944327a779bdbb2aa9
Jessica Li <liqqj@DESKTOP-ANPB8GG.localdomain>
2022-10-31 18:51:20 -0400

    prettier

--- backend/src/entities/MealDelivery.ts
+++ backend/src/entities/MealDelivery.ts
@@ -1,15 +1,15 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 // Creates an entity
 // Entity is a class that maps to a database table : https://orkhan.gitbook.io/typeorm/docs/entities
 @Entity()
 export class MealDeliveryEntity {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Column()
-	quantity: number;
+  @Column()
+  quantity: number;
 
-	@Column()
-	mealType: string;
+  @Column()
+  mealType: string;
 }
