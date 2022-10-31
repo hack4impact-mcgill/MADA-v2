4c12e749912c01f283c36ef23080d77b0faa588c
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2022-10-31 18:23:26 -0400

    Updated data-source.ts

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,19 +1,19 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { AdminEntity } from "./entities/AdminEntity";
-import { ExampleEntity } from "./ExampleEntity";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { AdminEntity } from './entities/AdminEntity';
+import { ExampleEntity } from './ExampleEntity';
 
 // Create a data source i.e connection settings: https://orkhan.gitbook.io/typeorm/docs/data-source#what-is-datasource
 export const AppDataSource = new DataSource({
-	type: "postgres",
-	host: "localhost",
-	port: 5432,
-	username: "test",
-	password: "test",
-	database: "test",
-	synchronize: true,
-	logging: false,
-	entities: [ExampleEntity, AdminEntity],
-	migrations: [],
-	subscribers: [],
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'test',
+  password: 'test',
+  database: 'test',
+  synchronize: true,
+  logging: false,
+  entities: [ExampleEntity, AdminEntity],
+  migrations: [],
+  subscribers: []
 });
