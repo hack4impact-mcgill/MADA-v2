6452f36d79fea6b8ac52e7603d536c56187a63e9
KihyeokK <joe.brighter20@gmail.com>
2022-10-31 19:51:52 -0400

    Remove comment for VolunteerEntity

--- backend/src/entities/VolunteerEntity.ts
+++ backend/src/entities/VolunteerEntity.ts
@@ -1,8 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 import { UserEntity } from './UserEntity';
 
-// Creates an entity
-// Entity is a class that maps to a database table : https://orkhan.gitbook.io/typeorm/docs/entities
 @Entity()
 export class VolunteerEntity extends UserEntity {
   @PrimaryGeneratedColumn()
