4c81d07e934c277f79a530527fb168971d610437
KihyeokK <joe.brighter20@gmail.com>
2022-10-31 18:57:05 -0400

    Change Volunteer class before pulling for UserEntity

--- backend/src/VolunteerEntity.ts
+++ backend/src/VolunteerEntity.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 // Creates an entity
 // Entity is a class that maps to a database table : https://orkhan.gitbook.io/typeorm/docs/entities
 @Entity()
-export class Volunteer extends User {
+export class Volunteer extends UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
