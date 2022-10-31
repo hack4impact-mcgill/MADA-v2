94d924961bb953f35789bd7174bd58f5b18688a1
jessLii <qian.q.li@mail.mcgill.ca>
2022-10-31 19:51:45 -0400

    remove comment because it was not serving

--- backend/src/entities/MealDelivery.ts
+++ backend/src/entities/MealDelivery.ts
@@ -1,7 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-// Creates an entity
-// Entity is a class that maps to a database table : https://orkhan.gitbook.io/typeorm/docs/entities
 @Entity()
 export class MealDeliveryEntity {
   @PrimaryGeneratedColumn()
