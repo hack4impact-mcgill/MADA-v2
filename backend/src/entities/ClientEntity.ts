fb2f2e34d4cecf81922fbe4eaafb32ce80958e0f
Hamza Farooq <hamzafarooq360@gmail.com>
2022-11-07 18:37:49 -0500

    prettier

--- backend/src/entities/ClientEntity.ts
+++ backend/src/entities/ClientEntity.ts
@@ -1,9 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 import { UserEntity } from './UserEntity';
 
-// Creates an entity
-// Entity is a class that maps to a database table : https://orkhan.gitbook.io/typeorm/docs/entities
-
 @Entity()
 export class ClientEntity extends UserEntity{
   @PrimaryGeneratedColumn()
