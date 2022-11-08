144b151dbf99117cb18fd0da05f2d8c6cf4c6805
KihyeokK <joe.brighter20@gmail.com>
2022-11-07 19:41:44 -0500

    Add getAllTasks

---
+++ backend/src/controller/TaskController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Task } from "../entity/TaskEntity";
+
+export class TaskController {
+    private TaskRepository = AppDataSource.getRepository(Task);
+
+    // GET all tasks from database
+    getAllTasks = async (request: Request, response: Response) => {
+        try {
+            const tasks = await this.TaskRepository.find();
+            response.status(200).json({tasks: tasks});
+        } catch {
+            // internal server error, cannot query tasks
+            response.status(500);
+        }
+    }
+
+    // GET tasks filtered with parameters
+    getFilteredTasks = async (request: Request, response: Response) => {
+        const id = request.params.id; //assuming the URI will be /tasks/:id using path parameters
+        
+    }
+
+
+    // POST create a task
+}
