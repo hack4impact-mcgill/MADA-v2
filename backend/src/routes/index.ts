bf9841472a13f9709b98de821ed4168285020e7b
jessLii <qian.q.li@mail.mcgill.ca>
2022-11-07 20:58:09 -0500

    set up routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import { router as tasks } from './task.routes';
+import { adminRouter} from "./admin.route";
 
 // Create a router object
 export const api = express.Router();
 
 // Adds the routes from todo.route to this router
 api.use(tasks);
+api.use(adminRouter);
