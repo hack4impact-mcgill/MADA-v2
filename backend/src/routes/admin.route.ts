964d108439826caa65b661a433478a599af87cdd
Jessica Li <qian.q.li@mail.mcgill.ca>
2022-11-07 21:03:54 -0500

    prettier

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -1,10 +1,10 @@
-import * as express from "express";
-import { AdminController } from "../controller/AdminController";
+import * as express from 'express';
+import { AdminController } from '../controller/AdminController';
 
 export const adminRouter = express.Router();
 
 const adminController = new AdminController();
 
-adminRouter.get("/admin/users", adminController.getAllUsers);
-adminRouter.post("/admin/create", adminController.createUser);
-adminRouter.delete("/admin/delete", adminController.deleteUser);
+adminRouter.get('/admin/users', adminController.getAllUsers);
+adminRouter.post('/admin/create', adminController.createUser);
+adminRouter.delete('/admin/delete', adminController.deleteUser);
