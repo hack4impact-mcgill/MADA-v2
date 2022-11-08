58320d5b1f53fc9b69970435a243b05510de0f2d
KihyeokK <joe.brighter20@gmail.com>
2022-11-07 19:53:29 -0500

    Fix getAllTasks

--- backend/src/controller/TaskController.ts
+++ backend/src/controller/TaskController.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../data-source";
-import { Task } from "../entity/TaskEntity";
+import { TaskEntity } from "../entities/TaskEntity";
 
 export class TaskController {
-    private TaskRepository = AppDataSource.getRepository(Task);
+    private TaskRepository = AppDataSource.getRepository(TaskEntity);
 
     // GET all tasks from database
     getAllTasks = async (request: Request, response: Response) => {
         try {
             const tasks = await this.TaskRepository.find();
-            response.status(200).json({tasks: tasks});
+            response.status(200).json({tasks});
         } catch {
             // internal server error, cannot query tasks
             response.status(500);
@@ -19,7 +19,6 @@ export class TaskController {
     // GET tasks filtered with parameters
     getFilteredTasks = async (request: Request, response: Response) => {
         const id = request.params.id; //assuming the URI will be /tasks/:id using path parameters
-        
     }
 
 
