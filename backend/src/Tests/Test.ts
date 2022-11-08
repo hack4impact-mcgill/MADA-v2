e6d51ccf651bad1a4a9d0b5bc467f0e6df96d32f
Hamza Farooq <hamzafarooq360@gmail.com>
2022-11-07 19:30:05 -0500

    added test.ts

--- backend/src/Tests/Test.ts
+++ backend/src/Tests/Test.ts
@@ -1,1 +1,11 @@
-import { describe, expect, test } from '@jest/globals';
+import {expect, jest, test} from '@jest/globals';
+import {sum} from './sum'
+
+// example test //
+describe('sum module', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(sum(1, 2)).toBe(3);
+    });
+  });
+
+
