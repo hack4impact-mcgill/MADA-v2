295b2e975d4b3080fec256739821b07be7ba941a
KihyeokK <joe.brighter20@gmail.com>
2022-11-07 19:02:44 -0500

    Add VolunteerEntity to data-source.ts file

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -5,6 +5,7 @@ import { DataSource } from 'typeorm';
 import { UserEntity } from './entities/UserEntity';
 import { AdminEntity } from './entities/AdminEntity';
 import { ExampleEntity } from './ExampleEntity';
+import { VolunteerEntity } from './entities/VolunteerEntity';
 
 // Create a data source i.e connection settings: https://orkhan.gitbook.io/typeorm/docs/data-source#what-is-datasource
 export const AppDataSource = new DataSource({
@@ -16,7 +17,7 @@ export const AppDataSource = new DataSource({
   database: 'test',
   synchronize: true,
   logging: false,
-  entities: [ExampleEntity, UserEntity, AdminEntity],
+  entities: [ExampleEntity, UserEntity, AdminEntity, VolunteerEntity],
   migrations: [],
   subscribers: []
 });
