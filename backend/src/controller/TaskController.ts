12159b912396f18f6a9d9ec74939ff153481dfcf
KihyeokK <joe.brighter20@gmail.com>
2022-11-14 19:40:32 -0500

    Finished all 6 apis for the TaskController

--- backend/src/controller/TaskController.ts
+++ backend/src/controller/TaskController.ts
@@ -9,7 +9,7 @@ export class TaskController {
   getAllTasks = async (request: Request, response: Response) => {
     try {
       const tasks = await this.TaskRepository.find();
-      response.status(200).json({ tasks: tasks });
+      response.status(200).json({ tasks });
     } catch {
       // internal server error, cannot query tasks
       response.status(500);
@@ -18,8 +18,62 @@ export class TaskController {
 
   // GET tasks filtered with parameters
   getFilteredTasks = async (request: Request, response: Response) => {
-    const id = request.params.id; //assuming the URI will be /tasks/:id using path parameters
+    try {
+      const tasks = await this.TaskRepository.find({ where: request.body });
+      response.status(200).json({ tasks });
+    } catch {
+      response.status(500);
+    }
   };
 
   // POST create a task
+  createTask = async (request: Request, response: Response) => {
+    try {
+      const task = await this.TaskRepository.save(request.body);
+      response.status(200).json({ task });
+    } catch {
+      response.status(500);
+    }
+  };
+
+  // GET a specific task
+  getTask = async (request: Request, response: Response) => {
+      try {
+        const task = await this.TaskRepository.findOneBy({
+          id: parseInt(request.params.id)
+        });
+        response.status(200).json({ task });
+      } catch {
+        response.status(500);
+      }
+    }
+
+  // PUT update a task
+  updateTask = async (request: Request, response: Response) => {
+    try {
+      const taskToUpdate = await this.TaskRepository.findOneBy({
+        id: parseInt(request.params.id)
+      });
+      const task = await this.TaskRepository.save({
+        ...taskToUpdate,
+        ...request.body
+      });
+      response.status(200).json({ task });
+    } catch {
+      response.status(500);
+    }
+  };
+
+  // DELETE a task
+  deleteTask = async (request: Request, response: Response) => {
+    try {
+      const taskToDelete = await this.TaskRepository.findOneBy({
+        id: parseInt(request.params.id)
+      });
+      const task = await this.TaskRepository.delete(taskToDelete);
+      response.status(200).json({ task });
+    } catch {
+      response.status(500);
+    }
+  };
 }
