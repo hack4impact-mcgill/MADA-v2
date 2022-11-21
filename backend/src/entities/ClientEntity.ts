9c90d005c0e0a03eb4777b1ae8368ede90b2813c
Hamza Farooq <hamzafarooq360@gmail.com>
2022-11-21 18:25:54 -0500

    ran prettier

--- backend/src/entities/ClientEntity.ts
+++ backend/src/entities/ClientEntity.ts
@@ -2,24 +2,23 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 import { UserEntity } from './UserEntity';
 
-
 @Entity()
 export class ClientEntity extends UserEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column()
-    address: string;
-  
-    @Column()
-    email: string;
-  
-    @Column()
-    phoneNumber: Number;
-  
-    @Column()
-    name: string;
-  
-    @Column()
-    notes: string;
-  }
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  address: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phoneNumber: Number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  notes: string;
+}
