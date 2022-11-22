225b4281976918b5a9e75f7daf3c3ddcca3715db
Peter Rong <peter.gh.rong@gmail.com>
2022-11-21 20:19:36 -0500

    WIP

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,8 @@
+import * as express from 'express';
+import { router as tasks } from './task.routes';
+
+// Create a router object
+export const api = express.Router();
+
+// Adds the routes from todo.route to this router
+api.use(tasks);
--- backend/tests/tasks.test.ts
+++ backend/tests/tasks.test.ts
@@ -1,16 +1,26 @@
 import { TaskEntity } from '../src/entities/TaskEntity';
 import { AppDataSource } from '../src/data-source';
+import TaskController from '../src/controllers/tasks';
+import * as request from 'supertest';
+import app from '../src/index';
 
 describe('Tasks tests', () => {
+  const taskController: TaskController = new TaskController();
+  const taskRepository = AppDataSource.getRepository(TaskEntity);
+
+  beforeEach(() => {
+    taskRepository.clear();
+  });
+
   it('should update test', async () => {
     // save a new task
     const TaskRepository = AppDataSource.getRepository(TaskEntity);
     const newTask = new TaskEntity();
-    newTask.deliveryTime = new Date('December 17, 1995 03:24:00');
+    newTask.deliveryTime = new Date('April 20, 2001 04:20:00');
     newTask.deliveries = [];
     newTask.isCompleted = false;
     const savedTask = await TaskRepository.save(newTask);
 
-    // to finish
+    request(app).update();
   });
 });
