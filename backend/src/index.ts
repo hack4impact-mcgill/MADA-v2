75088cff748937a74b0ff93686c44d19fe2af748
Peter Rong <peter.gh.rong@gmail.com>
2022-11-21 20:19:36 -0500

    WIP

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,18 +6,22 @@ import * as cors from 'cors';
 // Import from other .ts files
 import { AppDataSource } from './data-source';
 import { resolve } from 'path';
+import { api } from './routes/index';
+
+// Create express app
+const app = express();
 
 AppDataSource.initialize().then(async () => {
-  // Create express app
-  const app = express();
-
   // Using third party middleware
   app.use(bodyParser.json()); // body-parser is which allows express to read the body and then parse that into a Json object that we can understand.
   app.use(cors()); //
 
   // Routes go here
   // Creating route : https://expressjs.com/en/guide/routing.html
+  app.use('/api', api);
 
   // Starts server and listens on port 3001 for connections
   app.listen(3001);
 });
+
+export default app;
