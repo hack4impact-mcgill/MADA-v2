f2aa89dc74c1180bdee24c474f54ba9c4a968e88
Sophearah <sophearah.suy-puth@mail.mcgill.ca>
2022-11-28 19:00:15 -0500

    removed some conflict shit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c7dc21a4fa3677d7bbcdca2c1e6c975350e8ae14
 // Import modules from node_modules
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
@@ -22,28 +18,3 @@ app.use(cors()); //
 app.use('/api', api);
 
 export default app;
-<<<<<<< HEAD
-=======
-// Import modules from node_modules
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
-
-// Import from other .ts files
-import { api } from './routes/index';
-
-// Create express app
-const app = express();
-
-// Using third party middleware
-app.use(bodyParser.json()); // body-parser is which allows express to read the body and then parse that into a Json object that we can understand.
-app.use(cors()); //
-
-// Routes go here
-// Creating route : https://expressjs.com/en/guide/routing.html
-app.use('/api', api);
-
-export default app;
->>>>>>> separated app and index, fixed test, added test util
-=======
->>>>>>> c7dc21a4fa3677d7bbcdca2c1e6c975350e8ae14
