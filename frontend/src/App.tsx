0c0e7275d9f2341758f360e9580853bf9c4c0fc7
Stéphanie Fontaine <74389720+steph-fontaine@users.noreply.github.com>
2023-01-30 17:17:04 -0500

    Components Folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Components/Header/Header';
 import RouterComponent from './Router';
 
 // Create a functional component
 export default function App() {
 	return (
 		<div>
+			<Header/>
 			<RouterComponent/>
 		</div>
 	);
---
+++ frontend/src/Components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'; 
+
+
+const Header = ()=> {
+    return(
+        <div className='header'>
+            <button className='help-button'>Help</button>
+        </div>
+    )
+}
+
+export default Header; 
+
+
+
