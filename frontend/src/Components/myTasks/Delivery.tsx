ea8676ea17893ab5e508df9db88203d3f04d44ce
KihyeokK <joe.brighter20@gmail.com>
2023-01-31 00:09:42 -0500

    Partially finish Delivery component, add styles

--- frontend/src/Components/myTasks/Delivery.tsx
+++ frontend/src/Components/myTasks/Delivery.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Box, FormControlLabel, Checkbox } from "@mui/material";
+import { Box, FormControlLabel, Checkbox, Typography } from "@mui/material";
+import DeliveryLabel from "./DeliveryLabel";
 
-const Delivery = (props: { isCompleted: Boolean }) => {
+const Delivery = (props: { isCompleted: boolean, deliveryTime: Date, name: string}) => {
   return (
-    <Box>
+    <Box sx={{ display: 'flex', fontFamily: "Poppins", bgcolor: "#FFFFFF", height: 99, width: 'auto' }}>
       <FormControlLabel
-        control={<Checkbox checked={true} /*onChange*/ />}
-        label='John Doe'
+        control={
+          <Checkbox
+            sx={{ "& .MuiSvgIcon-root": { fontSize: 36 }, ml: 1 }}
+            checked={true} /*onChange*/
+          />
+        }
+        label={<DeliveryLabel isCompleted={props.isCompleted} deliveryTime={props.deliveryTime} name={props.name}/>}
       />
     </Box>
   );
--- frontend/src/Containers/TasksContainter.tsx
+++ frontend/src/Containers/TasksContainter.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import Delivery from '../Components/myTasks/Delivery';
+import React from "react";
+import Delivery from "../Components/myTasks/Delivery";
+import './TasksContainer.css';
 
+const TasksContainer = () => {
+  return (
+    <div className="tasks-container">
+      Hello this is the tasks page
+      <Delivery isCompleted={true} deliveryTime={new Date()} name={'Jane Doe'}></Delivery>
+      <Delivery isCompleted={false} deliveryTime={new Date()} name={'Leopold Bennett'}></Delivery>
+      <Delivery isCompleted={true} deliveryTime={new Date()} name={'Avi Sharp'}></Delivery>
+      <Delivery isCompleted={true} deliveryTime={new Date()} name={'Zahara Lott'}></Delivery>
+    </div>
+  );
+};
 
-const TasksContainer=()=>{
-    return(
-        <div>Hello this is the tasks page
-            <Delivery isCompleted={true}></Delivery>   
-        </div>
-    )
-}
-
-export default TasksContainer; 
+export default TasksContainer;
