f69ff531fc49f6654eaf108d3a47ef32503bcd3f
Hamza Farooq <hamzafarooq360@gmail.com>
2023-02-08 16:18:21 -0500

    added css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import User from './User';
-import Availibilities from './Availibilities';
-import Account from './Account';
-import Contact from './Contact';
-import Footer from './Footer';
 
 // Create a functional component
 export default function App() {
 	return (
 		<div>
 			<User/>
-            <Availibilities/>
-            <Account/>
-            <Contact/>
-            <Footer/>
 		</div>
 	);
 }
