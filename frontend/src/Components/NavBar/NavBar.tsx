b0b30357f9eb2f3c6f1eca7486ea903a31939f14
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2023-02-22 18:25:23 -0500

    added horizontal line

--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -4,11 +4,15 @@ import { BsCalendarCheck, BsCardList, BsFillPersonFill } from "react-icons/bs";
 
 
 const NavBar = ()=> {
+
     return(
         <div className='navBar'>
-            <button className='today'> <BsCalendarCheck className='icon'/> <br/> Today</button>
-            <button className='MyTasks'> <BsCardList className='icon'/> <br/> My Tasks</button>
-            <button className='Me'> <BsFillPersonFill className='icon'/> <br/> Me</button>
+            <hr ></hr>
+            <div className='horizontalBar'>
+                <button> <BsCalendarCheck className='icon'/> <br/> Today</button>
+                <button> <BsCardList className='icon'/> <br/> My Tasks</button>
+                <button> <BsFillPersonFill className='icon'/> <br/> Me</button>
+            </div>
         </div>
     )
 }
