bfb40479a76f26f4f29a71d5b7f4ea313f242b70
Hamza Farooq <hamzafarooq360@gmail.com>
2023-02-22 16:01:38 -0500

    deleted footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import RouterComponent from './Router';
+import UserProfileContainer from './Containers/UserProfileContainer';
 
 // Create a functional component
 export default function App() {
