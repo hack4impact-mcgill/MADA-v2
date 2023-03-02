a9fb3b1a33143f8bb271cfccf545466dbe0d8b98
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2023-03-02 13:24:41 -0500

    Added onClick event functions

--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -5,13 +5,25 @@ import { BsCalendarCheck, BsCardList, BsFillPersonFill } from "react-icons/bs";
 
 const NavBar = ()=> {
 
+    const onClickToday = async () => {
+        window.location.href = '/today';
+    };
+
+    const onClickTasks = async () => {
+        window.location.href = '/tasks';
+    };
+
+    const onClickMe = async () => {
+        window.location.href = '/profile';
+    };
+
     return(
         <div className='navBar'>
             <hr ></hr>
             <div className='horizontalBar'>
-                <button> <BsCalendarCheck className='icon'/> <br/> Today</button>
-                <button> <BsCardList className='icon'/> <br/> My Tasks</button>
-                <button> <BsFillPersonFill className='icon'/> <br/> Me</button>
+                <button onClick={onClickToday}> <BsCalendarCheck className='icon'/> <br/> Today</button>
+                <button onClick={onClickTasks}> <BsCardList className='icon'/> <br/> My Tasks</button>
+                <button onClick={onClickMe}> <BsFillPersonFill className='icon'/> <br/> Me</button>
             </div>
         </div>
     )
