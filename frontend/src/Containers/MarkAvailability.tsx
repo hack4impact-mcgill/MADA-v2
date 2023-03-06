95076a52af6f443ab3dbe69bab0639091f833c73
Selena Jin <selenajin9@gmail.com>
2023-03-05 20:20:22 -0500

    basic functions working

--- frontend/src/Containers/MarkAvailability.tsx
+++ frontend/src/Containers/MarkAvailability.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Dayjs } from "dayjs";
 import {
-  Stack,
+  Box,
   TextField,
   Accordion,
   AccordionSummary,
@@ -12,59 +12,64 @@ import {
 } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { ExpandMore, FormatAlignJustify } from "@mui/icons-material";
+
+import { MdDeleteOutline, MdExpandMore } from "react-icons/md";
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 type TimePickerAccordionProps = {
-  prop: string;
-  setTime: (dayOfWeek: string, time: Dayjs) => void;
   dayOfWeek: string;
 };
-
-// pass in day of week as prop
-const TimePickerAccordion = ({
-  prop,
-  setTime,
-  dayOfWeek,
-}: TimePickerAccordionProps) => {
-  return (
-    <Accordion id={prop}>
-      <Stack
-        direction="row"
-        sx={{
-          alignItems: "center",
-        }}
-      >
-        <Switch />
-        <AccordionSummary
-          expandIcon={<ExpandMore />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          sx={{ flexGrow: 1 }}
-        >
-          <Typography>{prop}</Typography>
-        </AccordionSummary>
-      </Stack>
-      <AccordionDetails>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </Typography>
-      </AccordionDetails>
-    </Accordion>
-  );
+type TimeRange = {
+  startTime: Dayjs | null;
+  endTime: Dayjs | null;
 };
 
 const MarkAvailability = () => {
-  const [startDate, setStartDate] = useState<Dayjs | null>(null);
-  const [endDate, setEndDate] = useState<Dayjs | null>(null);
-  const [monday, setMonday] = useState<Array<Dayjs>>([]);
-  const [tuesday, setTuesday] = useState<Array<Dayjs>>([]);
-  const [wednesday, setWednesday] = useState<Array<Dayjs>>([]);
-  const [thursday, setThursday] = useState<Array<Dayjs>>([]);
-  const [friday, setFriday] = useState<Array<Dayjs>>([]);
-  const [saturday, setSaturday] = useState<Array<Dayjs>>([]);
-  const [sunday, setSunday] = useState<Array<Dayjs>>([]);
+  const [monday, setMonday] = useState<TimeRange[]>([
+    {
+      startTime: null,
+      endTime: null,
+    },
+  ]);
+  const [tuesday, setTuesday] = useState<TimeRange[]>([
+    {
+      startTime: null,
+      endTime: null,
+    },
+  ]);
+  const [wednesday, setWednesday] = useState<TimeRange[]>([
+    {
+      startTime: null,
+      endTime: null,
+    },
+  ]);
+  const [thursday, setThursday] = useState<TimeRange[]>([
+    {
+      startTime: null,
+      endTime: null,
+    },
+  ]);
+  const [friday, setFriday] = useState<TimeRange[]>([
+    {
+      startTime: null,
+      endTime: null,
+    },
+  ]);
+  const [saturday, setSaturday] = useState<TimeRange[]>([
+    {
+      startTime: null,
+      endTime: null,
+    },
+  ]);
+  const [sunday, setSunday] = useState<TimeRange[]>([
+    {
+      startTime: null,
+      endTime: null,
+    },
+  ]);
+
   const daysOfWeek = [
     "Monday",
     "Tuesday",
@@ -75,59 +80,224 @@ const MarkAvailability = () => {
     "Sunday",
   ];
 
-  function setTime(dayOfWeek: string, time: Dayjs) {
+  function setTimes(dayOfWeek: string, time: TimeRange[]) {
     switch (dayOfWeek) {
       case "Monday":
-        setMonday([...monday, time]);
+        setMonday(time);
         break;
       case "Tuesday":
-        setTuesday([...tuesday, time]);
+        setTuesday(time);
         break;
       case "Wednesday":
-        setWednesday([...wednesday, time]);
+        setWednesday(time);
         break;
       case "Thursday":
-        setThursday([...thursday, time]);
+        setThursday(time);
         break;
       case "Friday":
-        setFriday([...friday, time]);
+        setFriday(time);
         break;
       case "Saturday":
-        setSaturday([...saturday, time]);
+        setSaturday(time);
         break;
       case "Sunday":
-        setSunday([...sunday, time]);
+        setSunday(time);
         break;
     }
   }
 
+  function GetTimes(dayOfWeek: string) {
+    switch (dayOfWeek) {
+      case "Monday":
+        return monday;
+      case "Tuesday":
+        return tuesday;
+      case "Wednesday":
+        return wednesday;
+      case "Thursday":
+        return thursday;
+      case "Friday":
+        return friday;
+      case "Saturday":
+        return saturday;
+      case "Sunday":
+        return sunday;
+      default:
+        return monday;
+    }
+  }
+
+  // pass in day of week as prop, containing the accordion associated to that day
+  const TimePickerAccordion = ({ dayOfWeek }: TimePickerAccordionProps) => {
+    const [shouldRender, setShouldRender] = React.useState<boolean>(false);
+
+    useEffect(() => {
+      console.log("hellooo");
+      if (shouldRender) {
+        setShouldRender(false);
+      }
+    }, [shouldRender]);
+    // contains a pair of time pickers, and the icons associated to them
+    const TimeRange = ({ index }: { index: number }) => {
+      const [startTime, setStartTime] = React.useState<Dayjs | null>(
+        GetTimes(dayOfWeek)[index].startTime
+      );
+      const [endTime, setEndTime] = React.useState<Dayjs | null>(
+        GetTimes(dayOfWeek)[index].endTime
+      );
+
+      return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <TimePicker
+              label="Start time"
+              value={startTime}
+              onChange={(startTime) => {
+                var tmp: TimeRange[] = GetTimes(dayOfWeek);
+                tmp[index].startTime = startTime;
+                setTimes(dayOfWeek, tmp);
+                setStartTime(startTime);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <TimePicker
+              label="End time"
+              value={endTime}
+              onChange={(endTime) => {
+                var tmp: TimeRange[] = GetTimes(dayOfWeek);
+                tmp[index].endTime = endTime;
+                setTimes(dayOfWeek, tmp);
+                setEndTime(endTime);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            {/* delete button */}
+            <MdDeleteOutline
+              size="40"
+              onClick={() => {
+                var tmp: TimeRange[] = GetTimes(dayOfWeek);
+                if (tmp.length == 1) {
+                  tmp[0].startTime = null;
+                  tmp[0].endTime = null;
+                } else {
+                  tmp.splice(index, 1);
+                }
+                setTimes(dayOfWeek, tmp);
+                setStartTime(null);
+                setEndTime(null);
+                setShouldRender(true);
+              }}
+            />
+            {/* add button  if it's the last element*/}
+            <IoIosAddCircleOutline
+              size="40"
+              visibility={
+                index == GetTimes(dayOfWeek).length - 1 ? "visible" : "hidden"
+              }
+              onClick={() => {
+                var tmp: TimeRange[] = GetTimes(dayOfWeek);
+                tmp.push({
+                  startTime: null,
+                  endTime: null,
+                });
+                setTimes(dayOfWeek, tmp);
+                setShouldRender(true);
+              }}
+            />
+          </Box>
+        </LocalizationProvider>
+      );
+    };
+
+    const [disabled, setDisabled] = useState(false);
+    const [expanded, setExpanded] = useState(false);
+
+    const ToggleAccordionDisability = (
+      event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      setDisabled(!event.target.checked);
+      console.log("hiii");
+      if (!event.target.checked) setExpanded(false);
+    };
+
+    const handleChange = (event: React.SyntheticEvent, isExpanded: boolean) => {
+      console.log("hello");
+      setExpanded(isExpanded);
+    };
+
+    return (
+      <Accordion
+        disabled={disabled}
+        expanded={expanded}
+        onChange={handleChange}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Switch checked={!disabled} onChange={ToggleAccordionDisability} />
+          <AccordionSummary
+            expandIcon={<MdExpandMore />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            sx={{ flexGrow: 1 }}
+          >
+            <Typography>{dayOfWeek}</Typography>
+          </AccordionSummary>
+        </Box>
+        <AccordionDetails>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: "column",
+              gap: 2,
+            }}
+          >
+            {GetTimes(dayOfWeek).map((day, i) => {
+              return <TimeRange index={i} />;
+            })}
+          </Box>
+        </AccordionDetails>
+      </Accordion>
+    );
+  };
+
+  const DateRangerPicker = () => {
+    const [startDate, setStartDate] = useState<Dayjs | null>(null);
+    const [endDate, setEndDate] = useState<Dayjs | null>(null);
+    return (
+      <>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <DatePicker
+            label="Start Date"
+            value={startDate}
+            onChange={(newValue) => {
+              setStartDate(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+
+          <DatePicker
+            label="End Date"
+            value={endDate}
+            onChange={(newValue) => {
+              setEndDate(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Box>
+      </>
+    );
+  };
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Stack id="date-range" direction="row">
-        <DatePicker
-          label="Start Date"
-          value={startDate}
-          onChange={(newValue) => {
-            setStartDate(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-
-        <DatePicker
-          label="End Date"
-          value={endDate}
-          onChange={(newValue) => {
-            setEndDate(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </Stack>
-
+      <DateRangerPicker></DateRangerPicker>
       {/* iterate through list of days of week and create accordion for each day */}
       {daysOfWeek.map((day) => {
-        return (
-          <TimePickerAccordion prop={day} setTime={setTime} dayOfWeek={day} />
-        );
+        return <TimePickerAccordion dayOfWeek={day} />;
       })}
     </LocalizationProvider>
   );
