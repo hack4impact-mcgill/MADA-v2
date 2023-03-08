1b2507d07c02af05e9a28c9374aceab2ba202946
jessLii <qian.q.li@mail.mcgill.ca>
2023-03-08 16:43:10 -0500

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import MarkAvailability from "./Containers/MarkAvailability";
+import LoginContainer from "./Containers/LoginContainer";
 import RouterComponent from "./Router";
 
 // Create a functional component
 export default function App() {
   return (
-    <div>
-      <MarkAvailability />
-    </div>
+    RouterComponent()
   );
 }
