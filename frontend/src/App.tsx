4a26aecbe664a8bba3bcc4327e31f6d7122afa46
Hamza Farooq <hamzafarooq360@gmail.com>
2023-03-19 14:02:50 -0400

    fixed merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import RouterComponent from './Router';
-import UserProfileContainer from './Containers/UserProfileContainer';
 
 // Create a functional component
 export default function App() {
