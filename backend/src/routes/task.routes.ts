57b64a0e69aff401f92f23e10a9d391a26ee1bce
Peter Rong <peter.gh.rong@gmail.com>
2023-03-22 15:36:47 -0400

    add get all tasks (#97)
    Get all tasks

--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -6,6 +6,7 @@ export const router = express.Router();
 const taskController = new TaskController();
 
 router.get('/tasks/:id', taskController.getTask);
+router.get('/tasks', taskController.getTasks);
 router.put('/tasks/:id', taskController.updateOrCreateTask);
 router.put('/tasks/', taskController.updateOrCreateTask);
 router.delete('/tasks/:id', taskController.deleteTask);
