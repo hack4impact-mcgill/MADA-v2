fe9e9082aac6452dec34f364b3a2a8a325e5ee53
Selena Jin <selenajin9@gmail.com>
2023-03-29 21:07:55 -0400

    fix style

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,34 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import AvailabilitiesContainer from './Containers/AvailabilitiesContainer';
-import HistoryContainer from './Containers/HistoryContainer';
-import LoginContainer from './Containers/LoginContainer';
-import TasksContainer from './Containers/TasksContainter';
-import TodayContainer from './Containers/TodayContainer';
-import UserProfileContainer from './Containers/UserProfileContainer';
-import NavBar from './Components/NavBar/NavBar';
-import ForgotPasswordContainer from './Containers/ForgotPasswordContainer';
+import React from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import AvailabilitiesContainer from "./Containers/AvailabilitiesContainer";
+import HistoryContainer from "./Containers/HistoryContainer";
+import LoginContainer from "./Containers/LoginContainer";
+import TasksContainer from "./Containers/TasksContainter";
+import TodayContainer from "./Containers/TodayContainer";
+import UserProfileContainer from "./Containers/UserProfileContainer";
+import NavBar from "./Components/NavBar/NavBar";
+import ForgotPasswordContainer from "./Containers/ForgotPasswordContainer";
 
+const RouterComponent = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/today" element={<TodayContainer />} />
+        <Route path="/" element={<LoginContainer />} />
+        <Route path="/password" element={<ForgotPasswordContainer />} />
+        <Route path="/profile" element={<UserProfileContainer />} />
+        <Route path="/history" element={<HistoryContainer />} />
+        <Route path="/tasks" element={<TasksContainer />} />
+        <Route path="/availabilities" element={<AvailabilitiesContainer />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-const RouterComponent=()=>{
-    return(
-        <BrowserRouter>
-        <div>
-          {
-            location.pathname !== "/" && location.pathname !== "/password" && <NavBar/>
-          }
-        <Routes>
-          <Route path="/today" element={<TodayContainer/>} />
-          <Route path="/" element={<LoginContainer />} />
-          <Route path="/password" element={<ForgotPasswordContainer />} />
-          <Route path="/profile" element={<UserProfileContainer/>}/>
-          <Route path="/history" element={<HistoryContainer/>}/>
-          <Route path="/tasks" element={<TasksContainer/>}/>
-          <Route path="/availabilities" element={<AvailabilitiesContainer/>}/>
-        </Routes>
-        </div>
-      </BrowserRouter>
-    )
-}
-
-export default RouterComponent; 
+export default RouterComponent;
