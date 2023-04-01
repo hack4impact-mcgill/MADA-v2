5756ad7082fab589de5ed23dd3e9a73f4aecd7e5
jessLii <qian.q.li@mail.mcgill.ca>
2023-04-01 17:35:54 -0400

    add client controller

---
+++ backend/src/routes/client.routes.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+import ClientController from '../controllers/client';
+
+// Create a router object
+export const router = express.Router();
+const clientController = new ClientController();
+
+router.get('/client/:id', clientController.getClient);
+router.put(
+  '/client/:id',
+  clientController.updateOrCreateClient
+);
+router.put(
+  '/client/',
+  clientController.updateOrCreateClient
+);
+router.delete('/client/:id', clientController.deleteClient);
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import { router as tasks } from './task.routes';
 import { router as mealDelivery } from './mealDelivery.routes';
 import { router as volunteers } from './volunteer.routes';
+import { router as client} from './client.routes';
 
 // Create a router object
 export const api = express.Router();
@@ -10,3 +11,4 @@ export const api = express.Router();
 api.use(tasks);
 api.use(mealDelivery);
 api.use(volunteers);
+api.use(client);
