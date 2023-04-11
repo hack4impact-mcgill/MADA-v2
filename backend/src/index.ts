b64a9c9e0161a219d3785b395977c8cf85e58bc3
Sophearah <sophearah.suy-puth@mail.mcgill.ca>
2023-04-11 19:59:43 +0100

    basic auth login for admin panel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,13 +7,40 @@ const AdminJSExpress = require('@adminjs/express')
 
 AdminJS.registerAdapter(TypeormAdapter);
 
+// Admin auth
+// Hardcoded basic auth: https://medium.com/adminjs/overriding-the-login-view-in-adminjs-has-been-simplified-bfd02eaac062
+// TODO - auth from db similar to this: https://docs.adminjs.co/installation/plugins/express#authenticated
+const cookiePassword = 'very_secret_secret'
+
+const DEFAULT_ADMIN = {
+    email: 'admin@example.com',
+    password: 'password',
+}
+
+const authenticate = async () => {
+    return { email: DEFAULT_ADMIN.email }
+}
+
 const run = async () => {
     // Initalizes data source
     await AppDataSource.initialize();
-    
+
     // Setup admin panel
     const admin = new AdminJS(adminConfig)
-    const adminRouter = AdminJSExpress.buildRouter(admin)
+    const adminRouter = AdminJSExpress.buildAuthenticatedRouter(admin,
+        {
+            authenticate,
+            cookiePassword: 'very_secret_secret',
+        },
+        null,
+        {
+            resave: true,
+            saveUninitialized: true,
+            cookiePassword,
+        },
+    )
+
+    // Start admin panel
     app.use(admin.options.rootPath, adminRouter)
 
     // Express app listens to port 3001
