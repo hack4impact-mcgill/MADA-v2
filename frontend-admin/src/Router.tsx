075a9667fcc5f3e73afb2385c087ec6b02cb4267
Sophearah <sophearah.suy-puth@mail.mcgill.ca>
2023-04-24 01:53:57 +0100

    auto redirect

--- frontend-admin/src/Router.tsx
+++ frontend-admin/src/Router.tsx
@@ -1,16 +1,38 @@
-import React from "react";
-import { Route, Routes} from "react-router-dom";
+import React, {useEffect} from "react";
+import {Box} from '@mui/material';
+import { Route, Routes, useNavigate } from "react-router-dom";
 import VolunteersPage from './components/volunteers/page';
 import ClientsPage from './components/clients/page';
 import TasksPage from './components/tasks/page';
 import LoginPage from './components/auth/page';
 import {Private} from './components/auth/private';
 
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+const RedirectPath = () => {
+    const navigate = useNavigate();
+    const token = cookies.get("TOKEN");
+
+    useEffect(() => {
+        if (token) {
+            navigate('/volunteers')
+        } else {
+            navigate('/login');
+        }
+    }, []);
+
+    return (<>
+        <Box>Redirecting...</Box>
+    </>)
+}
+
 const Router = () => {
     return (
         <Routes>
             <Route path='/login' element={<LoginPage />}/>
             <Route element={<Private />}>
+                <Route path="*" element={<RedirectPath/>} />
                 <Route path="/volunteers" element={<VolunteersPage/>} />
                 <Route path="/clients" element={<ClientsPage/>} />
                 <Route path="/tasks" element={<TasksPage/>} />
