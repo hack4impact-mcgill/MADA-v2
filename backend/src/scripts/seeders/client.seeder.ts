068030bb3d375427fedf47c3dc0c573080569841
Sophearah <sophearah.suy-puth@mail.mcgill.ca>
2023-05-08 17:08:43 +0100

    auto create route deliveries via seeder

--- backend/src/scripts/seeders/client.seeder.ts
+++ backend/src/scripts/seeders/client.seeder.ts
@@ -2,14 +2,27 @@ import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { DataSource } from 'typeorm';
 import { faker } from '@faker-js/faker';
 import { ClientEntity } from '../../entities/ClientEntity';
+import { RouteDeliveryEntity } from '../../entities/RouteDeliveryEntity';
+import { ProgramType } from '../../entities/types';
+
 import { generateStaffUser } from './user';
+
+const generateRouteDelivery = async (program: ProgramType, client: ClientEntity) => {
+    const routeDelivery = new RouteDeliveryEntity();
+    routeDelivery.routeNumber = 0;
+    routeDelivery.routePosition = 0;
+    routeDelivery.client = client;
+    routeDelivery.mealType = client.mealType;
+    routeDelivery.program = program;
+    return routeDelivery
+}
 
 const generateClient = async () => {
   const client = (await generateStaffUser()) as any;
   client.address = faker.address.streetAddress();
   client.mealType = faker.helpers.arrayElement(['vegetarian', 'nofish', 'nomeat'])
   client.sts = faker.datatype.boolean();
-  client.map = faker.datatype.boolean();
+  client.map = !client.sts ? true : faker.datatype.boolean();
   return client;
 };
 
@@ -30,5 +43,19 @@ export default class ClientSeeder implements Seeder {
     const repository = dataSource.getRepository(ClientEntity);
     const clients = await generateClients(10);
     await repository.insert(clients);
+
+    clients.forEach(async (client) => {
+        const routeDeliveryRepository = dataSource.getRepository(RouteDeliveryEntity);
+
+        if (client.sts) {
+            const stsRouteDelivery = await generateRouteDelivery(ProgramType.STS, client);
+            await routeDeliveryRepository.insert(stsRouteDelivery);
+        }
+    
+        if (client.map) {
+            const mapRouteDelivery = await generateRouteDelivery(ProgramType.MAP, client);
+            await routeDeliveryRepository.insert(mapRouteDelivery);
+        }
+    })
   }
 }
