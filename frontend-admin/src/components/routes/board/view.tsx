b477030ecc7330e22ab33d368302fde570963814
Sophearah <sophearah.suy-puth@mail.mcgill.ca>
2023-05-09 13:05:46 +0100

    view all routes

--- frontend-admin/src/components/routes/board/view.tsx
+++ frontend-admin/src/components/routes/board/view.tsx
@@ -6,10 +6,12 @@ import {Grid, Box} from '@mui/material'
 export const ViewBoard = (props: {groupedRoutes: any}) => {
     return (
         <Box sx={{display: 'flex', overflow: 'auto'}}>
-            <BoardList header={""} routes={props.groupedRoutes[0]}/>
-            {/* <BoardList header={"Route 1"}/>
-            <BoardList header={"Route 2"}/>
-            <BoardList header={"Route 3"}/> */}
+            <BoardList header={"Unassigned"} routes={props.groupedRoutes[0]}/>
+            {
+                Object.keys(props.groupedRoutes).slice(1).map((key, index) => 
+                    <BoardList header={"Route" + key} routes={props.groupedRoutes[key]}/>
+                )
+            }
         </Box>
     )
 }
