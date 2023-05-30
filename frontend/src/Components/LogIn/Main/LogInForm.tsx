1c992f045c258fdb21f0b836266cb585e2da7a15
Peter Rong <peter.gh.rong@gmail.com>
2023-05-30 19:32:14 -0400

    adding auth

--- frontend/src/Components/LogIn/Main/LogInForm.tsx
+++ frontend/src/Components/LogIn/Main/LogInForm.tsx
@@ -1,7 +1,7 @@
 import React, { useReducer, ChangeEvent } from "react";
 import { initialState } from "../../../Contexts/LogIn";
 import Reducer from "../../../Contexts/LogIn";
-import { Stack, Box } from "@mui/material";
+import { Stack, Box, Button } from "@mui/material";
 import RememberMeSwitch from "./RememberMeSwitch";
 import UsernameTextField from "../UsernameTextField";
 import PasswordTextField from "./PasswordTextField";
@@ -44,52 +44,74 @@ const LogInForm = () => {
     });
   };
 
-  const handleClickLogIn = async (e: React.FormEvent<HTMLFormElement>) => {
+  function handleClickLogIn() {
     //Prevent page reload
-    e.preventDefault();
+    // e.preventDefault();
     console.log("sign in");
     console.log(username);
     console.log(password);
-    console.log(rememberMe);
+
+    // console.log(rememberMe);
     //TODO backend
 
     // temporary redirect to today page
-    window.location.href = "/today";
-  };
+    // window.location.href = "/today";
+  }
 
   return (
     <Stack spacing={5} maxWidth="md">
-      <form onSubmit={handleClickLogIn}>
-        <Stack spacing={5}>
-          <Box display="flex" align-items="center" justifyContent="center">
-            <UsernameTextField
-              errorText="" //TODO error text from backend if wrong username/password
-              helperText="Incorrect username or password. Please try again."
-              placeHolder="Username or Email"
-              updateUsername={handleUsernameChange}
-              username={username}
-            />
-          </Box>
-          <Box display="flex" align-items="center" justifyContent="center">
-            <PasswordTextField
-              errorText="" //TODO error text from backend if wrong username/password
-              updatePassword={handlePasswordChange}
-              password={password}
-              handleClickShowPassword={handleClickShowPassword}
-              showPassword={showPassword}
-            />
-          </Box>
-          <Box display="flex" justifyContent="flex-end">
-            <RememberMeSwitch
-              updateRememberMeSwitch={handleChangeRememberMeSwitch}
-              isRememberMeChecked={rememberMe}
-            />
-          </Box>
-          <Box display="flex" justifyContent="center">
-            <SignInButton />
-          </Box>
-        </Stack>
-      </form>
+      {/* <form onSubmit={handleClickLogIn}> */}
+      <Stack spacing={5}>
+        <Box display="flex" align-items="center" justifyContent="center">
+          <UsernameTextField
+            errorText="" //TODO error text from backend if wrong username/password
+            helperText="Incorrect username or password. Please try again."
+            placeHolder="Username or Email"
+            updateUsername={handleUsernameChange}
+            username={username}
+          />
+        </Box>
+        <Box display="flex" align-items="center" justifyContent="center">
+          <PasswordTextField
+            errorText="" //TODO error text from backend if wrong username/password
+            updatePassword={handlePasswordChange}
+            password={password}
+            handleClickShowPassword={handleClickShowPassword}
+            showPassword={showPassword}
+          />
+        </Box>
+        <Box display="flex" justifyContent="flex-end">
+          <RememberMeSwitch
+            updateRememberMeSwitch={handleChangeRememberMeSwitch}
+            isRememberMeChecked={rememberMe}
+          />
+        </Box>
+        <Box display="flex" justifyContent="center">
+          <Button
+            className="login-button"
+            type="submit"
+            variant="contained"
+            sx={{
+              fontFamily: "Poppins",
+              fontStyle: "normal",
+              fontWeight: 600,
+              fontSize: isBrowser ? "17px" : "12px",
+              color: "#FFFFFF",
+              backgroundColor: "#2E5CD3",
+              borderRadius: "10px",
+              height: isBrowser ? "50px" : "40px",
+              width: "75%",
+              position: "center",
+            }}
+            onClick={() => {
+              handleClickLogIn();
+            }}
+          >
+            Sign in
+          </Button>
+        </Box>
+      </Stack>
+      {/* </form> */}
       <ForgotPasswordButton />
       <Box display="flex" justifyContent="center">
         <MADALogo />
