014fd2202042b9cd8db2d15fbb07e9eea773ced1
jessLii <qian.q.li@mail.mcgill.ca>
2023-06-13 19:32:33 -0400

    Update the meal delivery controller

--- backend/src/controllers/mealDelivery.ts
+++ backend/src/controllers/mealDelivery.ts
@@ -3,11 +3,13 @@ import { AppDataSource } from '../data-source';
 import { MealDeliveryEntity } from '../entities/MealDeliveryEntity';
 import { TaskEntity } from '../entities/TaskEntity';
 import { StatusCode } from './statusCode';
+import { ClientEntity } from '../entities/ClientEntity';
 
 export default class MealDeliveryController {
   private MealDeliveryRepository =
     AppDataSource.getRepository(MealDeliveryEntity);
   private TaskRepository = AppDataSource.getRepository(TaskEntity);
+  private ClientRepository = AppDataSource.getRepository(ClientEntity);
 
   getMealDeliveries = async (request: Request, response: Response) => {
     const meals = await this.MealDeliveryRepository.find({});
@@ -33,29 +35,45 @@ export default class MealDeliveryController {
     if (!request.params.id) {
       const newMealDelivery = new MealDeliveryEntity();
       newMealDelivery.mealType = request.body.mealType;
+      newMealDelivery.isCompleted = request.body.isCompleted;
+      newMealDelivery.routePosition = request.body.routePosition;
+      newMealDelivery.program = request.body.program;
       newMealDelivery.task = request.body.task
         ? await this.TaskRepository.findOneBy({
             id: request.body.task.id
           })
         : null;
+      newMealDelivery.client = request.body.client
+        ? await this.ClientRepository.findOneBy({
+          id: request.body.client.id
+        }) : null;
+      
       await this.MealDeliveryRepository.save(newMealDelivery);
       const mealDelivery = await this.MealDeliveryRepository.findOne({
         where: {
           id: newMealDelivery.id
         },
         relations: {
-          task: true
+          task: true,
+          client: true,
         }
       });
       response.status(StatusCode.OK).json({ mealDelivery: mealDelivery });
     } else {
       await this.MealDeliveryRepository.save({
         id: parseInt(request.params.id),
+        isCompleted: request.body.isCompleted,
+        routePosition: request.body.routePosition,
         mealType: request.body.mealType,
-        quantity: request.body.quantity,
+        program: request.body.program,
         task: request.body.task
-          ? await this.MealDeliveryRepository.findOneBy({
+          ? await this.TaskRepository.findOneBy({
               id: parseInt(request.body.task.id)
+            })
+          : null,
+        client: request.body.client
+          ? await this.ClientRepository.findOneBy({
+              id: parseInt(request.body.client.id)
             })
           : null
       });
@@ -64,7 +82,8 @@ export default class MealDeliveryController {
           id: parseInt(request.params.id)
         },
         relations: {
-          task: true
+          task: true,
+          client: true
         }
       });
       response.status(StatusCode.OK).json({ mealDelivery: mealDelivery });
