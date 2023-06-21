ef8e95622f3d5204f604c326e6930789556cccda
Selena Jin <selenajin9@gmail.com>
2023-06-20 21:19:56 -0400

    fix bug

--- frontend/src/helper.ts
+++ frontend/src/helper.ts
@@ -1,13 +1,14 @@
 import { Volunteer } from "./Contexts/Volunteer";
 
 export function getCurrentUserId(): string | null | undefined {
+  var pattern = /^\/passwordReset/;
   try {
     const user = sessionStorage.getItem("userId");
     if (
       !user &&
       window.location.pathname !== "/" &&
       window.location.pathname !== "/password" &&
-      window.location.pathname !== "/passwordreset"
+      !pattern.test(window.location.pathname)
     ) {
       window.location.href = "/";
       return null;
