5a9661d5d4e380b98264a0316cf2fb7616034fff
sophearah <sophearahspsp@gmail.com>
2023-07-03 17:22:44 -0700

    add everything from current main into here

--- backend/ormconfig.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  entities: ['**/entities/*.{js,ts}']
-};
--- backend/tests/example.test.ts
+++
@@ -1,6 +0,0 @@
-import { describe, expect, test } from '@jest/globals';
-import { sum } from './example/example';
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
