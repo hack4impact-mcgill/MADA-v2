49ae0c81caadaa30569add9dd9bcc34ec620da04
Sophearah Suy-Puth <sophearahspsp@gmail.com>
2024-03-21 14:34:35 +0700

    basic modal setup for creating tasks

--- frontend-admin/src/components/tasks/page.tsx
+++ frontend-admin/src/components/tasks/page.tsx
@@ -7,12 +7,47 @@ import {BasePage} from 'src/components/common/layout/base-page'
 import {Box} from '@mui/material'
 import {DataGrid} from '@mui/x-data-grid'
 import { taskColumns } from './columns'
+import { ModalControl } from "src/components/common/modal/control";
+import { useModalState } from "src/components/common/modal/use-modal-state";
+import { CreateModal } from "./modals";
+import { ActionBar } from "src/components/common/layout/page-actionbar";
 
 const TasksPage = () => {
     const { isLoading, isError, data, error } = useQuery(['tasks'], () => getTasks())
+    const {
+        state: createModal,
+        handleOpen: handleOpenCreateModal,
+        handleClose: handleCloseCreateModal,
+    } = useModalState();
     
+    const Header = () => {
+        return (
+          <ActionBar
+            actions={[
+              {
+                handler: handleOpenCreateModal,
+                label: "Create Tasks",
+              },
+            ]}
+          />
+        );
+    };
+
+    const CreateModalControl = () => {
+        return (
+            <ModalControl
+                {...{
+                status: createModal,
+                handleClose: handleCloseCreateModal,
+                children: <CreateModal handleClose={handleCloseCreateModal} />,
+                }}
+            />
+        );
+    };
+
     return (
-        <BasePage header={<div></div>}>
+        <BasePage header={<Header/>}>
+            <CreateModalControl />
             <Box sx={{display: 'flex', flexDirection: 'column', width: '100%', height: '85%'}}>                
                 <DataGrid
                     rows={data ? data!.data.tasks : []}
@@ -22,6 +57,6 @@ const TasksPage = () => {
             </Box>
         </BasePage>
     )
-}
+} 
 
 export default TasksPage;
